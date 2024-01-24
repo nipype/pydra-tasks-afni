28a56bba1a7087a659ab82d2e2e548fc159634fa
Tom Close <tom.g.close@gmail.com>
2024-01-24 15:44:57 +1100

    imported latest generated package contents

--- related-packages/fileformats-extras/fileformats/extras/medimage_afni/__init__.py
+++ related-packages/fileformats-extras/fileformats/extras/medimage_afni/__init__.py
@@ -3,71 +3,65 @@ import typing as ty
 from random import Random
 from fileformats.core import FileSet
 from fileformats.medimage_afni import (
-    Oned,
-    Dset,
-    Nii[0],
-    Threed,
-    Head,
-    Ncorr,
-    R1,
     All1,
-    Oned,
     Unit errts+tlrc,
-    Gii,
+    Oned,
+    Oned,
+    Nii[0],
+    Ncorr,
+    Head,
+    Threed,
+    Dset,
+    R1,
 )
 
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_oned_data(oned: Oned, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_all1_data(all1: All1, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_dset_data(dset: Dset, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_unit errts+tlrc_data(unit errts+tlrc: Unit errts+tlrc, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_nii[0]_data(nii[0]: Nii[0], dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_oned_data(oned: Oned, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_threed_data(threed: Threed, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_oned_data(oned: Oned, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_head_data(head: Head, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_nii[0]_data(nii[0]: Nii[0], dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_ncorr_data(ncorr: Ncorr, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_ncorr_data(ncorr: Ncorr, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_r1_data(r1: R1, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_head_data(head: Head, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_all1_data(all1: All1, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_threed_data(threed: Threed, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_oned_data(oned: Oned, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_dset_data(dset: Dset, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_unit errts+tlrc_data(unit errts+tlrc: Unit errts+tlrc, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
-    raise NotImplementedError
-
-
-@FileSet.generate_sample_data.register
-def gen_sample_gii_data(gii: Gii, dest_dir: Path, seed: ty.Union[int, Random], stem: ty.Optional[str]):
+def gen_sample_r1_data(r1: R1, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
     raise NotImplementedError
--- related-packages/fileformats/fileformats/medimage_afni/__init__.py
+++ related-packages/fileformats/fileformats/medimage_afni/__init__.py
@@ -1,47 +1,7 @@
 from fileformats.generic import File
 
-class Oned(File):
-    ext = ".1d"
-    binary = True
-
-
-class Dset(File):
-    ext = ".dset"
-    binary = True
-
-
-class Nii[0](File):
-    ext = ".nii[0]"
-    binary = True
-
-
-class Threed(File):
-    ext = ".3D"
-    binary = True
-
-
-class Head(File):
-    ext = ".HEAD"
-    binary = True
-
-
-class Ncorr(File):
-    ext = ".ncorr"
-    binary = True
-
-
-class R1(File):
-    ext = ".r1"
-    binary = True
-
-
 class All1(File):
     ext = ".all1"
-    binary = True
-
-
-class Oned(File):
-    ext = ".1D"
     binary = True
 
 
@@ -50,6 +10,41 @@ class Unit errts+tlrc(File):
     binary = True
 
 
-class Gii(File):
-    ext = ".gii"
+class Oned(File):
+    ext = ".1d"
+    binary = True
+
+
+class Oned(File):
+    ext = ".1D"
+    binary = True
+
+
+class Nii[0](File):
+    ext = ".nii[0]"
+    binary = True
+
+
+class Ncorr(File):
+    ext = ".ncorr"
+    binary = True
+
+
+class Head(File):
+    ext = ".HEAD"
+    binary = True
+
+
+class Threed(File):
+    ext = ".3D"
+    binary = True
+
+
+class Dset(File):
+    ext = ".dset"
+    binary = True
+
+
+class R1(File):
+    ext = ".r1"
     binary = True
