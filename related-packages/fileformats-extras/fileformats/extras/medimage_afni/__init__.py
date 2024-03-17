d6d716ddd0009a273dfa7aad92a83285716369fe
Tom Close <tom.g.close@gmail.com>
2024-03-17 18:42:05 +1100

    updated fileformats

--- related-packages/fileformats-extras/fileformats/extras/medimage_afni/__init__.py
+++ related-packages/fileformats-extras/fileformats/extras/medimage_afni/__init__.py
@@ -3,65 +3,70 @@ import typing as ty
 from random import Random
 from fileformats.core import FileSet
 from fileformats.medimage_afni import (
-    All1,
-    Unit errts+tlrc,
-    Oned,
-    Oned,
-    Nii[0],
-    Ncorr,
+    OneD,
     Head,
-    Threed,
-    Dset,
+    All1,
     R1,
+    ThreeD,
+    Dset,
 )
 
 
-
 @FileSet.generate_sample_data.register
-def gen_sample_all1_data(all1: All1, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_oned_data(
+    oned: OneD,
+    dest_dir: Path,
+    seed: ty.Union[int, Random] = 0,
+    stem: ty.Optional[str] = None,
+) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_unit errts+tlrc_data(unit errts+tlrc: Unit errts+tlrc, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_head_data(
+    head: Head,
+    dest_dir: Path,
+    seed: ty.Union[int, Random] = 0,
+    stem: ty.Optional[str] = None,
+) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_oned_data(oned: Oned, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_all1_data(
+    all1: All1,
+    dest_dir: Path,
+    seed: ty.Union[int, Random] = 0,
+    stem: ty.Optional[str] = None,
+) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_oned_data(oned: Oned, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_r1_data(
+    r1: R1,
+    dest_dir: Path,
+    seed: ty.Union[int, Random] = 0,
+    stem: ty.Optional[str] = None,
+) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_nii[0]_data(nii[0]: Nii[0], dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_threed_data(
+    threed: ThreeD,
+    dest_dir: Path,
+    seed: ty.Union[int, Random] = 0,
+    stem: ty.Optional[str] = None,
+) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_ncorr_data(ncorr: Ncorr, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
-    raise NotImplementedError
-
-
-@FileSet.generate_sample_data.register
-def gen_sample_head_data(head: Head, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
-    raise NotImplementedError
-
-
-@FileSet.generate_sample_data.register
-def gen_sample_threed_data(threed: Threed, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
-    raise NotImplementedError
-
-
-@FileSet.generate_sample_data.register
-def gen_sample_dset_data(dset: Dset, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
-    raise NotImplementedError
-
-
-@FileSet.generate_sample_data.register
-def gen_sample_r1_data(r1: R1, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_dset_data(
+    dset: Dset,
+    dest_dir: Path,
+    seed: ty.Union[int, Random] = 0,
+    stem: ty.Optional[str] = None,
+) -> ty.Iterable[Path]:
     raise NotImplementedError
--- related-packages/fileformats/fileformats/medimage_afni/__init__.py
+++ related-packages/fileformats/fileformats/medimage_afni/__init__.py
@@ -1,50 +1,32 @@
 from fileformats.generic import File
 
-class All1(File):
-    ext = ".all1"
-    binary = True
-
-
-class Unit errts+tlrc(File):
-    ext = ".unit errts+tlrc"
-    binary = True
-
-
-class Oned(File):
-    ext = ".1d"
-    binary = True
-
-
-class Oned(File):
-    ext = ".1D"
-    binary = True
-
-
-class Nii[0](File):
-    ext = ".nii[0]"
-    binary = True
-
-
-class Ncorr(File):
-    ext = ".ncorr"
-    binary = True
-
 
 class Head(File):
     ext = ".HEAD"
     binary = True
 
 
-class Threed(File):
-    ext = ".3D"
+class All1(File):
+    ext = ".all1"
     binary = True
 
 
-class Dset(File):
-    ext = ".dset"
+class OneD(File):
+    ext = ".1D"
     binary = True
 
 
 class R1(File):
     ext = ".r1"
+    binary = True
+
+
+class ThreeD(File):
+    ext = ".3D"
+    binary = True
+    alternate_exts = (".3d",)
+
+
+class Dset(File):
+    ext = ".dset"
     binary = True
